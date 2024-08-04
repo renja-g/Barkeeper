e6707373f2424094c2b2775fcd57cd76ca4c3b52
renja <renja.grotemeyer@gmail.com>
2024-08-04 20:18:17 +0200

    chore: Refactor code structure and fix bugs in icon selection

--- commands/link_account.go
+++ commands/link_account.go
@@ -35,26 +35,30 @@ var link_account = discord.SlashCommandCreate{
 			Name:        "region",
 			Description: "Your League of Legends region.",
 			Required:    true,
-			Choices: []discord.ApplicationCommandOptionChoiceString{
-				{Name: "BR (Brazil)", Value: "BR1"},
-				{Name: "EUNE (Europe Nordic & East)", Value: "EUN1"},
-				{Name: "EUW (Europe West)", Value: "EUW1"},
-				{Name: "JP (Japan)", Value: "JP1"},
-				{Name: "KR (Korea)", Value: "KR"},
-				{Name: "LAN (Latin America North)", Value: "LA1"},
-				{Name: "LAS (Latin America South)", Value: "LA2"},
-				{Name: "NA (North America)", Value: "NA1"},
-				{Name: "OCE (Oceania)", Value: "OC1"},
-				{Name: "TR (Turkey)", Value: "TR1"},
-				{Name: "RU (Russia)", Value: "RU"},
-				{Name: "PH (Philippines)", Value: "PH2"},
-				{Name: "SG (Singapore)", Value: "SG2"},
-				{Name: "TH (Thailand)", Value: "TH2"},
-				{Name: "TW (Taiwan)", Value: "TW2"},
-				{Name: "VN (Vietnam)", Value: "VN2"},
-			},
+			Choices:     getRegionChoices(),
 		},
 	},
+}
+
+func getRegionChoices() []discord.ApplicationCommandOptionChoiceString {
+	return []discord.ApplicationCommandOptionChoiceString{
+		{Name: "BR (Brazil)", Value: "BR1"},
+		{Name: "EUNE (Europe Nordic & East)", Value: "EUN1"},
+		{Name: "EUW (Europe West)", Value: "EUW1"},
+		{Name: "JP (Japan)", Value: "JP1"},
+		{Name: "KR (Korea)", Value: "KR"},
+		{Name: "LAN (Latin America North)", Value: "LA1"},
+		{Name: "LAS (Latin America South)", Value: "LA2"},
+		{Name: "NA (North America)", Value: "NA1"},
+		{Name: "OCE (Oceania)", Value: "OC1"},
+		{Name: "TR (Turkey)", Value: "TR1"},
+		{Name: "RU (Russia)", Value: "RU"},
+		{Name: "PH (Philippines)", Value: "PH2"},
+		{Name: "SG (Singapore)", Value: "SG2"},
+		{Name: "TH (Thailand)", Value: "TH2"},
+		{Name: "TW (Taiwan)", Value: "TW2"},
+		{Name: "VN (Vietnam)", Value: "VN2"},
+	}
 }
 
 func LinkAccountHandler(cfg *dbot.Config) handler.SlashCommandHandler {
@@ -63,101 +67,105 @@ func LinkAccountHandler(cfg *dbot.Config) handler.SlashCommandHandler {
 		tagLine := data.String("tag_line")
 		region := data.String("region")
 
-		url := fmt.Sprintf("https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s", gameName, tagLine, cfg.RiotApiKey)
-		resp, err := http.Get(url)
+		riotResponse, err := verifyRiotAccount(cfg.RiotApiKey, gameName, tagLine)
 		if err != nil {
-			return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to verify account. Please try again later.").Build())
+			return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent(err.Error()).Build())
 		}
-		defer resp.Body.Close()
 
-		if resp.StatusCode == http.StatusOK {
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to read response. Please try again later.").Build())
-			}
+		summonerResponse, err := fetchSummonerData(cfg.RiotApiKey, riotResponse.PUUID, region)
+		if err != nil {
+			return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent(err.Error()).Build())
+		}
 
-			var riotResponse constants.RiotAccountResponse
-			err = json.Unmarshal(body, &riotResponse)
-			if err != nil {
-				return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to parse response. Please try again later.").Build())
-			}
+		accountData := constants.AccountData{
+			RiotAccountResponse: *riotResponse,
+			Region:              region,
+			VerifyImageID:       getRandomIcon(summonerResponse.ProfileIconId),
+		}
 
-			accountData := constants.AccountData{
-				RiotAccountResponse: riotResponse,
-				Region:              region,
-				// VerifyImageID:       randomIconID,
-			}
-			dataID := uuid.New().String()
+		dataID := uuid.New().String()
+		DataCache[dataID] = accountData
 
-			url := fmt.Sprintf("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/%s?api_key=%s", accountData.PUUID, cfg.RiotApiKey)
-			resp, err := http.Get(url)
-			if err != nil {
-				return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to verify account. Please try again later.").Build())
-			}
-			defer resp.Body.Close()
+		return sendVerificationMessage(e, accountData, dataID)
+	}
+}
 
-			if resp.StatusCode != http.StatusOK {
-				return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to fetch summoner data. Please try again later.").Build())
-			}
+func verifyRiotAccount(apiKey, gameName, tagLine string) (*constants.RiotAccountResponse, error) {
+	url := fmt.Sprintf("https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s", gameName, tagLine, apiKey)
+	return makeRequest[constants.RiotAccountResponse](url, "Failed to verify account")
+}
 
-			body, err = io.ReadAll(resp.Body)
-			if err != nil {
-				return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to read response. Please try again later.").Build())
-			}
+func fetchSummonerData(apiKey, puuid, region string) (*constants.SummonerResponse, error) {
+	url := fmt.Sprintf("https://%s.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/%s?api_key=%s", region, puuid, apiKey)
+	return makeRequest[constants.SummonerResponse](url, "Failed to fetch summoner data")
+}
 
-			var summonerResponse constants.SummonerResponse
-			err = json.Unmarshal(body, &summonerResponse)
-			if err != nil {
-				return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Failed to parse response. Please try again later.").Build())
-			}
+func makeRequest[T any](url, errorMessage string) (*T, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", errorMessage, err)
+	}
+	defer resp.Body.Close()
 
-			icons := map[int]string{
-				//7:  "DEBONAIR ROSE ICON",
-				//9:  "DAGGERS ICON",
-				//10: "WINGED SWORD ICON",
-				//12: "FULLY STACKED MEJAI'S ICON",
-				18: "MIX MIX ICON",
-				21: "TREE OF LIFE ICON",
-				//22: "REVIVE ICON",
-				//23: "LIL' SPROUT ICON",
-				//24:  "SPIKE SHIELD ICON",
-				//28: "TIBBERS ICON",
-			}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s: status code %d", errorMessage, resp.StatusCode)
+	}
 
-			// Extract keys from the map
-			keys := make([]int, 0, len(icons))
-			for k := range icons {
-				keys = append(keys, k)
-			}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response: %w", err)
+	}
 
-			// Generate a random index
-			randomIndex := rand.Intn(len(keys))
-			for keys[randomIndex] == summonerResponse.ProfileIconId {
-				keys[randomIndex] = rand.Intn(len(keys))
-			}
-			randomIconID := keys[randomIndex]
-			randomIconName := icons[randomIconID]
-			accountData.VerifyImageID = randomIconID
-			
-			DataCache[dataID] = accountData
-			
-			imageURL := fmt.Sprintf("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default//v1/profile-icons/%d.jpg", randomIconID)
+	var result T
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, fmt.Errorf("failed to parse response: %w", err)
+	}
 
-			embed := discord.NewEmbedBuilder().
-				SetTitle("Verify Account").
-				SetDescription(fmt.Sprintf("Account %s#%s has been found.\nChange you profile picture to the `%s` and click the verify button.", accountData.GameName, accountData.TagLine, randomIconName)).
-				SetImage(imageURL).
-				Build()
+	return &result, nil
+}
 
-			return e.CreateMessage(discord.NewMessageCreateBuilder().
-				SetEmbeds(embed).
-				AddActionRow(
-					discord.NewPrimaryButton("Verify", "verify_acc/"+dataID),
-				).
-				Build(),
-			)
-		} else {
-			return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Account not found or API error. Please check your details and try again.").Build())
+func getRandomIcon(currentIconId int) int {
+	icons := []int{18, 9, 10, 12, 21, 22, 23, 24, 28, 7}
+	for {
+		randomIconID := icons[rand.Intn(len(icons))]
+		if randomIconID != currentIconId {
+			return randomIconID
 		}
 	}
+}
+
+func sendVerificationMessage(e *handler.CommandEvent, accountData constants.AccountData, dataID string) error {
+	iconName := getIconName(accountData.VerifyImageID)
+	imageURL := fmt.Sprintf("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default//v1/profile-icons/%d.jpg", accountData.VerifyImageID)
+
+	embed := discord.NewEmbedBuilder().
+		SetTitle("Verify Account").
+		SetDescription(fmt.Sprintf("Account %s#%s has been found.\nChange your profile picture to the `%s` and click the verify button.", accountData.GameName, accountData.TagLine, iconName)).
+		SetImage(imageURL).
+		Build()
+
+	return e.CreateMessage(discord.NewMessageCreateBuilder().
+		SetEmbeds(embed).
+		AddActionRow(
+			discord.NewPrimaryButton("Verify", "verify_acc/"+dataID),
+		).
+		Build(),
+	)
+}
+
+func getIconName(iconID int) string {
+	icons := map[int]string{
+                7:  "DEBONAIR ROSE ICON",
+                9:  "DAGGERS ICON",
+                10: "WINGED SWORD ICON",
+                12: "FULLY STACKED MEJAI'S ICON",
+                18: "MIX MIX ICON",
+                21: "TREE OF LIFE ICON",
+                22: "REVIVE ICON",
+                23: "LIL' SPROUT ICON",
+                24:  "SPIKE SHIELD ICON",
+                28: "TIBBERS ICON",
+
+	}
+	return icons[iconID]
 }
