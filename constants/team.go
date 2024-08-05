bf5756bcb94b03a6f39ee9a72d79e1b114d373f4
renja <renja.grotemeyer@gmail.com>
2024-08-06 00:21:19 +0200

    formatting

--- constants/team.go
+++ constants/team.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Match struct {
-	MatchID   snowflake.ID    `json:"matchID"`
+	MatchID   snowflake.ID   `json:"matchID"`
 	Team1     []snowflake.ID `json:"team1"`
 	Team2     []snowflake.ID `json:"team2"`
-	Winner    string          `json:"winner"`
-	Timestamp int64           `json:"timestamp"`
+	Winner    string         `json:"winner"`
+	Timestamp int64          `json:"timestamp"`
 }
